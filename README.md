343c58b955742a17c15d32bf055cbad8d94db371
juan.cardona <felipe7u7432@gmail.com>
2023-11-05 18:21:49 -0500

    Agrega las transiciones al automata y se cambia archivo README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AFD_For_Password
 Programación y descripción de un autómata para validar una contraseña segura.
+
+## Reglas
+El autómata de contraseña que se piensa construir deberá soportar solo caracteres ASCII estándar y entre estos los símbolos que conformarán el alfabeto del autómata serán:
+
+∑ = {A-Z, a-z, 0-9, !, ¡, @, #, $, %, ^, &, *, (,), _, +, {, }, [, ], :, ;, <, >, , , ., ¿, ?, ~, -, \, /, =}
+Las reglas que el autómata deberá cumplir son las siguientes:
+-	Debe poseer por lo menos 4 caracteres
+-	Debe poseer por lo menos un número
+-	Debe poseer por lo menos una letra mayúscula
+-	Debe poseer por lo menos una letra minúscula
+-	No debe poseer espacios en blanco
+-	Debe poseer por lo menos un carácter especial. Los caracteres especiales permitidos son ∑ = {!, ¡, @, #, $, %, ^, &, *, (,), _, +, {, }, [, ], :, ;, <, >, , , ., ¿, ?, ~, -, \, /, =}
+-	Estos 4 caracteres que debe poseer obligatoriamente pueden estar ubicados en cualquier orden
+
+## Diagrama de Estados
+
--- automata.py
+++ automata.py
@@ -1,50 +1,84 @@
-import re
+uppercase = 'A-Z'
+number = '0-9'
+lowercase = 'a-z'
+specialCharacter = '!¡@#$%^&*()_+{}[]:;<>,.¿?~\\/='
 
-lowercase = r"^[a-z]+$"
-uppercase = r"^[A-Z]+$"
-number = r"^\d+$"
-specialCharacter = r"^[!¡@#\$%^&\*\(\)_\+\{\}\[\]:;<>,\.\¿\?~\\/\=]+$"
+statusFinal = 'q59'
+
+transitions = {
+    'q0': {uppercase: 'q1', lowercase: 'q2', number: 'q3', specialCharacter: 'q4'},
+    'q1': {uppercase: 'q1', lowercase: 'q40', number: 'q41', specialCharacter: 'q44'},
+    'q2': {uppercase: 'q11', lowercase: 'q2', number: 'q12', specialCharacter: 'q10'},
+    'q3': {uppercase: 'q8', lowercase: 'q13', number: 'q3', specialCharacter: 'q9'},
+    'q4': {uppercase: 'q5', lowercase: 'q6', number: 'q7', specialCharacter: 'q4'},
+    'q5': {uppercase: 'q5', lowercase: 'q29', number: 'q28', specialCharacter: 'q5'},
+    'q6': {uppercase: 'q32', lowercase: 'q6', number: 'q33', specialCharacter: 'q6'},
+    'q7': {uppercase: 'q36', lowercase: 'q37', number: 'q7', specialCharacter: 'q7'},
+    'q8': {uppercase: 'q8', lowercase: 'q51', number: 'q8', specialCharacter: 'q50'},
+    'q9': {uppercase: 'q57', lowercase: 'q56', number: 'q9', specialCharacter: 'q9'},
+    'q10': {uppercase: 'q13', lowercase: 'q10', number: 'q14', specialCharacter: 'q10'},
+    'q11': {uppercase: 'q11', lowercase: 'q11', number: 'q18', specialCharacter: 'q17'},
+    'q12': {uppercase: 'q22', lowercase: 'q12', number: 'q12', specialCharacter: 'q21'},
+    'q13': {uppercase: 'q53', lowercase: 'q13', number: 'q13', specialCharacter: 'q54'},
+    'q14': {uppercase: 'q59', lowercase: 'q14', number: 'q14', specialCharacter: 'q14'},
+    'q17': {uppercase: 'q17', lowercase: 'q17', number: 'q59', specialCharacter: 'q17'},
+    'q18': {uppercase: 'q18', lowercase: 'q18', number: 'q18', specialCharacter: 'q59'},
+    'q21': {uppercase: 'q59', lowercase: 'q21', number: 'q21', specialCharacter: 'q21'},
+    'q22': {uppercase: 'q22', lowercase: 'q22', number: 'q22', specialCharacter: 'q59'},
+    'q28': {uppercase: 'q28', lowercase: 'q59', number: 'q28', specialCharacter: 'q28'},
+    'q29': {uppercase: 'q29', lowercase: 'q29', number: 'q59', specialCharacter: 'q29'},
+    'q31': {uppercase: 'q31', lowercase: 'q31', number: 'q59', specialCharacter: 'q31'},
+    'q32': {uppercase: 'q32', lowercase: 'q32', number: 'q59', specialCharacter: 'q32'},
+    'q33': {uppercase: 'q59', lowercase: 'q33', number: 'q33', specialCharacter: 'q33'},
+    'q36': {uppercase: 'q36', lowercase: 'q36', number: 'q59', specialCharacter: 'q36'},
+    'q37': {uppercase: 'q59', lowercase: 'q37', number: 'q37', specialCharacter: 'q37'},
+    'q40': {uppercase: 'q40', lowercase: 'q40', number: 'q45', specialCharacter: 'q43'},
+    'q41': {uppercase: 'q41', lowercase: 'q49', number: 'q41', specialCharacter: 'q42'},
+    'q42': {uppercase: 'q42', lowercase: 'q59', number: 'q42', specialCharacter: 'q42'},
+    'q43': {uppercase: 'q43', lowercase: 'q43', number: 'q59', specialCharacter: 'q43'},
+    'q44': {uppercase: 'q44', lowercase: 'q46', number: 'q47', specialCharacter: 'q44'},
+    'q45': {uppercase: 'q45', lowercase: 'q45', number: 'q45', specialCharacter: 'q59'},
+    'q46': {uppercase: 'q46', lowercase: 'q46', number: 'q59', specialCharacter: 'q46'},
+    'q47': {uppercase: 'q47', lowercase: 'q47', number: 'q59', specialCharacter: 'q47'},
+    'q49': {uppercase: 'q49', lowercase: 'q49', number: 'q59', specialCharacter: 'q49'},
+    'q50': {uppercase: 'q50', lowercase: 'q59', number: 'q50', specialCharacter: 'q50'},
+    'q51': {uppercase: 'q51', lowercase: 'q51', number: 'q51', specialCharacter: 'q59'},
+    'q53': {uppercase: 'q53', lowercase: 'q53', number: 'q53', specialCharacter: 'q59'},
+    'q54': {uppercase: 'q59', lowercase: 'q54', number: 'q54', specialCharacter: 'q54'},
+    'q56': {uppercase: 'q59', lowercase: 'q56', number: 'q56', specialCharacter: 'q56'},
+    'q57': {uppercase: 'q57', lowercase: 'q57', number: 'q57', specialCharacter: 'q57'},
+    'q59': {uppercase: 'q59', lowercase: 'q59', number: 'q59', specialCharacter: 'q59'}
+}
 
 
-def validExpression(expression, symbol):
-    isValid = False
-    if re.match(expression, symbol):
-        isValid = True
-    return isValid
+def validateAlphabet(character):
+    inputChain = ''
+    if character.isupper():
+        inputChain = uppercase
+    elif character.islower():
+        inputChain = lowercase
+    elif character.isdigit():
+        inputChain = number
+    elif character in specialCharacter:
+        inputChain = specialCharacter
+    return inputChain
 
 
-class Automata:
-    def __init__(self):
-        self.currentStatus = 0
-
-    def transition(self, chain):
-        for symbol in chain:
-            if self.currentStatus == 0 and validExpression(specialCharacter, symbol):
-                self.currentStatus = 1
-            elif self.currentStatus == 0 and validExpression(uppercase, symbol):
-                self.currentStatus = 2
-            elif self.currentStatus == 0 and validExpression(lowercase, symbol):
-                self.currentStatus = 3
-            elif self.currentStatus == 0 and validExpression(number, symbol):
-                self.currentStatus = 4
-            else:
-                self.currentStatus = -1  # Estado de error
-
-    def accepted(self):
-        return self.currentStatus == 4
+def accepted(chain):
+    status = 'q0'
+    for character in chain:
+        inputChain = validateAlphabet(character)
+        if inputChain in transitions[status]:
+            status = transitions[status][inputChain]
+        else:
+            return False  # El caracter no es válido.
+    return status in {statusFinal}
 
 
-def main():
-    automata = Automata()
-    password = input("Ingresa una contaseña segura :D: ")
-
-    automata.transition(password)
-
-    if automata.accepted():
-        print("La cadena es aceptada.")
+testInput = 0
+while testInput == 0:
+    password = input('Ingresa una contraseña segura :D ')
+    if accepted(password):
+        print('\t La cadena es aceptada.')
     else:
-        print("La cadena no es aceptada.")
-
-
-if __name__ == "__main__":
-    main()
+        print('\t La cadena no es aceptada.')
